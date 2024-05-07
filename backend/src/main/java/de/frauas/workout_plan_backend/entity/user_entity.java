658779ebfd767ce8f7f0b005387fb3086884a4f2
kd0tp <kpulut23@gmail.com>
2024-05-07 12:02:54 +0200

    user_entity class erstellt

--- backend/src/main/java/de/frauas/workout_plan_backend/entity/user_entity.java
+++ backend/src/main/java/de/frauas/workout_plan_backend/entity/user_entity.java
@@ -1,7 +1,9 @@
 package de.frauas.workout_plan_backend.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
 
 @Entity
 public class user_entity {
