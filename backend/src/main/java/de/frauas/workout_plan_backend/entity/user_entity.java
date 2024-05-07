ff90b9f96cd98010cb7105ced5fbb903b2d85ee4
kd0tp <kpulut23@gmail.com>
2024-05-07 11:59:10 +0200

    user_entity class erstellt

--- backend/src/main/java/de/frauas/workout_plan_backend/entity/user_entity.java
+++ backend/src/main/java/de/frauas/workout_plan_backend/entity/user_entity.java
@@ -1,6 +1,59 @@
 package de.frauas.workout_plan_backend.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class user_entity {
 
-    int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String email_address;
+    private String username;
+    private String password;
+
+    public user_entity() {}
+
+    public user_entity(String password, String username, String email_address, int id) {
+        this.password = password;
+        this.username = username;
+        this.email_address = email_address;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail_address() {
+        return email_address;
+    }
+
+    public void setEmail_address(String email_address) {
+        this.email_address = email_address;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
 }
