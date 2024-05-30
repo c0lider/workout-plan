0c4385df36165d378da224443d6332ef05db1b6c
Oliver Seiffermann <oliver.seiffermann@dkd.de>
2024-05-30 15:40:17 +0200

    feat(react-bootstrap): setup react bootstrap

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 
 const Home = () => {
     return (
@@ -7,7 +8,7 @@ const Home = () => {
             <div className="col-12">
                 <h1 className="h1">This is our homepage</h1>
                 <Link to="/workouts">Workouts</Link>
-                <button className="btn btn-primary login-button">Login</button>
+                <Button>Login</Button>
             </div>
         </div>
     );
