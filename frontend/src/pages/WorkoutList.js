1f9f13486bae50fa76f4f86313816a7e73ba2980
Oliver Seiffermann <oliver.seiffermann@dkd.de>
2024-06-03 02:23:43 +0200

    feat(user landing page): create user landing page with dummy workout names, create scss classes for footer and adapt bootstrap variables
    Refs: #11

--- frontend/src/pages/WorkoutList.js
+++ frontend/src/pages/WorkoutList.js
@@ -1,17 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ListGroup from 'react-bootstrap/ListGroup';
 
+// TODO: remove dummy data and fetch data from backend
 const WorkoutList = () => {
+    const dummyWorkouts = [
+        {
+            name: 'Chest & Triceps',
+            id: 1,
+        },
+        {
+            name: 'Back & Biceps',
+            id: 2,
+        },
+        {
+            name: 'Legs',
+            id: 3,
+        },
+    ];
+
+    const [workouts, setWorkouts] = useState([]);
+    useEffect(() => {
+        // use dummy workouts
+        setWorkouts([...dummyWorkouts]);
+
+        // fetch('http://localhost:8000/api/workouts/')
+        //     .then((response) => response.json())
+        //     .then((data) => setWorkouts(data))
+        //     .catch((error) => console.error('Error:', error));
+    }, []);
+
     return (
-        <div className="container">
-            <div className="col-12">
-                <h1>This is our list of workouts</h1>
-                <ul>
-                    <li>test 1</li>
-                    <li>test 2</li>
-                    <li>test 3</li>
-                    <li>test 4</li>
-                    <li>test 5</li>
-                </ul>
+        <div className="workout-list-background p-5">
+            <div className="container">
+                <ListGroup as="ul">
+                    <ListGroup.Item as="li" className="workout-teaser" key={0}>
+                        <a
+                            href="/workouts/new"
+                            className="text-decoration-none text-primary"
+                        >
+                            <i className="fa-solid fa-circle-plus me-4"></i> Add
+                            routine
+                        </a>
+                    </ListGroup.Item>
+                    {workouts.map((workout) => (
+                        <ListGroup.Item
+                            as="li"
+                            className="workout-teaser"
+                            key={workout.id}
+                        >
+                            <a
+                                href={`workouts/${workout.id}`}
+                                className="text-decoration-none text-primary"
+                            >
+                                {workout.name}
+                            </a>
+                        </ListGroup.Item>
+                    ))}
+                </ListGroup>
             </div>
         </div>
     );
