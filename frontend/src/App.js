7b8d4060236222cc124ce3df1795d503d1c4beec
Oliver Seiffermann <oliver.seiffermann@dkd.de>
2024-06-11 12:33:16 +0200

    feat(404): add not found page
    Refs: #27

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import List from "./pages/WorkoutList";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import List from './pages/WorkoutList';
+import Login from './pages/Login';
+import Register from './pages/Register';
 import WorkoutDetails from './pages/WorkoutDetails';
-import Layout from "./layout/Layout";
+import Layout from './layout/Layout';
+import NotFound from './pages/NotFound';
 
 function App() {
-  return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/workouts" element={<List />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/workouts/:id" element={<WorkoutDetails />} />
-      </Routes>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/workouts" element={<List />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/workouts/:id" element={<WorkoutDetails />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </Layout>
+    );
 }
 
 export default App;
