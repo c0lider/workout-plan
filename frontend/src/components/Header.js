85739e348c9b3e36d3395bc48f256212d213f2b1
Oliver Seiffermann <oliver.seiffermann@dkd.de>
2024-06-11 12:45:14 +0200

    feat(header): make header stick to top
    Refs: #22

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,41 +1,45 @@
-import React from "react";
-import { Navbar, Nav, Button, Container } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
+import React from 'react';
+import { Navbar, Nav, Button, Container } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const handleLogin = () => {
-    navigate("/login");
-  };
+    const handleLogin = () => {
+        navigate('/login');
+    };
 
-  const handleRegister = () => {
-    navigate("/register");
-  };
+    const handleRegister = () => {
+        navigate('/register');
+    };
 
-  return (
-    <Navbar variant="dark" expand="lg" className="header">
-      <Container>
-        <Nav className="mr-auto">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="#">Programs</Nav.Link>
-          <Nav.Link href="#">About us</Nav.Link>
-        </Nav>
-        <Nav>
-          <Button
-            variant="outline-light"
-            className="mr-2"
-            onClick={handleLogin}
-          >
-            Login
-          </Button>
-          <Button variant="light" onClick={handleRegister}>
-            Register
-          </Button>
-        </Nav>
-      </Container>
-    </Navbar>
-  );
+    return (
+        <Navbar
+            variant="dark"
+            expand="lg"
+            className="header sticky-top bg-secondary"
+        >
+            <Container>
+                <Nav className="mr-auto">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/workouts">Programs</Nav.Link>
+                    <Nav.Link href="#">About us</Nav.Link>
+                </Nav>
+                <Nav>
+                    <Button
+                        variant="outline-light"
+                        className="mr-2"
+                        onClick={handleLogin}
+                    >
+                        Login
+                    </Button>
+                    <Button variant="light" onClick={handleRegister}>
+                        Register
+                    </Button>
+                </Nav>
+            </Container>
+        </Navbar>
+    );
 };
 
 export default Header;
