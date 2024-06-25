150d4a5e201ee54d8dde997d86226cfcad8cf890
Oliver Seiffermann <oliver.seiffermann@dkd.de>
2024-06-25 13:57:15 +0200

    feat(frontend): make cursor a pointer for add teasers

--- frontend/src/components/common/Teasers/AddTeaser.js
+++ frontend/src/components/common/Teasers/AddTeaser.js
@@ -6,7 +6,7 @@ const AddTeaser = ({ text, onClick }) => {
         <TextTeaser>
             <a
                 onClick={() => onClick()}
-                className="text-decoration-none text-primary"
+                className="text-decoration-none text-primary cursor-pointer"
             >
                 <i className="fa-solid fa-circle-plus me-4"></i>
                 {text}
